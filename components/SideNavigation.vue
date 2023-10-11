39e202b74e8e38bb6979b74016de85a717cbe12d
maliksvd <malik.krechiche@gmail.com>
2023-10-11 19:20:17 -0400

    feat: create SideNavigation component

---
+++ components/SideNavigation.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div
+      class="bottom-0 fixed md:flex justify-center bg-[#222223] h-auto md:h-screen md:border-r md:border-r-[#7C7F82]/20 border-t border-t-[#7C7F82]/20 w-full md:w-auto py-4 md:py-0 px-4">
+      <nav>
+        <ul class="flex md:flex-col justify-center items-center space-x-4 md:space-y-4 md:space-x-0 h-full">
+          <li>
+            <UTooltip text="Homepage" :ui="ui" :popper="{
+              placement: 'right',
+            }">
+              <NuxtLink to="/">
+                <UIcon name="i-solar-home-angle-broken" class="w-6 h-6" />
+              </NuxtLink>
+            </UTooltip>
+          </li>
+          <li>
+            <UTooltip text="All publications" :ui="ui" :popper="{
+              placement: 'right',
+            }">
+              <NuxtLink to="/publications">
+                <UIcon name="i-solar-book-2-broken" class="w-6 h-6" />
+              </NuxtLink>
+            </UTooltip>
+          </li>
+          <li>
+            <UTooltip text="Inspiration wall" :ui="ui" :popper="{
+              placement: 'right',
+            }">
+              <NuxtLink to="/inspiration">
+                <UIcon name="i-solar-album-broken" class="w-6 h-6" />
+              </NuxtLink>
+            </UTooltip>
+          </li>
+          <li>
+            <UTooltip text="Contact" :ui="ui" :popper="{
+              placement: 'right',
+            }">
+              <NuxtLink to="/contact">
+                <UIcon name="i-solar-mailbox-broken" class="w-6 h-6" />
+              </NuxtLink>
+            </UTooltip>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const ui = {
+  background: 'bg-white dark:bg-white',
+  color: 'text-black dark:text-black',
+}
+</script>
+
+
+
+<style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,11 @@
 <template>
-  <div class="w-full min-h-screen flex">
-    <div class="fixed flex justify-center bg-zinc-800/40 h-screen border-r border-r-[#7C7F82]/20">
-      <nav>
-        <ul class="flex flex-col justify-center items-center space-y-4 h-full">
-          <li class="mx-4">
-            <UTooltip text="Homepage" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/">
-                <UIcon name="i-solar-home-angle-broken" class="w-6 h-6" />
-              </NuxtLink>
-            </UTooltip>
-          </li>
-          <li class="mx-4">
-            <UTooltip text="All publications" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/publications">
-                <UIcon name="i-solar-book-2-broken" class="w-6 h-6" />
-              </NuxtLink>
-            </UTooltip>
-          </li>
-          <li class="mx-4">
-            <UTooltip text="Inspiration wall" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/inspiration">
-                <UIcon name="i-solar-album-broken" class="w-6 h-6" />
-              </NuxtLink>
-            </UTooltip>
-          </li>
-          <li class="mx-4">
-            <UTooltip text="Contact" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/contact">
-                <UIcon name="i-solar-mailbox-broken" class="w-6 h-6" />
-              </NuxtLink>
-            </UTooltip>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <main class="max-w-xs md:max-w-6xl mx-auto my-12">
+  <div class="w-full min-h-screen flex flex-col md:flex-row">
+    <SideNavigation />
+    <main class="max-w-sm md:max-w-6xl mx-auto my-12">
       <slot />
     </main>
   </div>
 </template>
 
-<script lang="ts" setup>
-const ui = {
-  background: 'bg-white dark:bg-white',
-  color: 'text-black dark:text-black',
-}
-</script>
 
 <style></style>

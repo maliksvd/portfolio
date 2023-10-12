2aea6f6ed09061fa8c6e639575b18cb9f5cf0aa2
maliksvd <malik.krechiche@gmail.com>
2023-10-11 22:50:38 -0400

    feat: add inspiration wall

---
+++ content/inspiration/test-2.md
@@ -0,0 +1,7 @@
+---
+title: "A test 22222"
+categories:
+  - "Nuxt"
+source: "https://dev.to/krutiepatel/5-tips-for-nuxt-3-development-1j0"
+image: "https://res.cloudinary.com/practicaldev/image/fetch/s--_4Z3Z3ZS--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2q0q1q1q1q1q1q1q1q1q.png"
+---
---
+++ pages/inspiration/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 class="font-semibold mb-2 text-xl">Inspiration Wall</h2>
+    <p class="text-[#7c7f82] text-sm">Discover a curated collection resources: links, tweets, articles, and more that
+      inspire me.</p>
+    <section class="mt-12 grid grid-cols-1 md:grid-cols-4 gap-8">
+      <ContentList path="/inspiration" v-slot="{ list }">
+        <NuxtLink v-for="inspiration in list" :key="inspiration._path" :to="inspiration._path">
+          <div class="bg-[#1c1c1c] h-48 w-full ring-[#282828] ring-1 rounded-lg mb-6"></div>
+          <h2 class="mt-2 text-sm font-medium">{{ inspiration.title }}</h2>
+        </NuxtLink>
+      </ContentList>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+<style></style>

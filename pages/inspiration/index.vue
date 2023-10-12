3d278066e0715d2f9f14e11e8a731e691ef9b015
maliksvd <malik.krechiche@gmail.com>
2023-10-11 23:37:02 -0400

    feat: add object-cover to image

--- pages/inspiration/index.vue
+++ pages/inspiration/index.vue
@@ -6,8 +6,11 @@
     <section class="mt-12 grid grid-cols-1 md:grid-cols-4 gap-8">
       <ContentList path="/inspiration" v-slot="{ list }">
         <NuxtLink v-for="inspiration in list" :key="inspiration._path" :to="inspiration._path">
-          <div class="bg-[#1c1c1c] h-48 w-full ring-[#282828] ring-1 rounded-lg mb-6"></div>
-          <h2 class="mt-2 text-sm font-medium">{{ inspiration.title }}</h2>
+          <div>
+            <div v-if="inspiration.image"><img :src="inspiration.image"
+                class="opacity-50 rounded-lg h-48 mb-6 object-cover" alt="inspiration.title"></div>
+            <div v-else class="bg-[#1c1c1c] h-48 w-full ring-[#282828] ring-1 rounded-lg mb-6"></div>
+          </div>
         </NuxtLink>
       </ContentList>
     </section>
@@ -15,6 +18,8 @@
 </template>
 
 <script lang="ts" setup>
+
+
 
 </script>
 

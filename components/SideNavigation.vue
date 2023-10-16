1436cb0981727e6306e404fdabd6f245c6426f97
maliksvd <malik.krechiche@gmail.com>
2023-10-16 00:00:41 -0400

    feat: improve navigation design

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -1,44 +1,34 @@
 <template>
   <div>
     <div
-      class="bottom-0 fixed md:flex justify-center bg-[#1c1c1c] h-auto md:h-screen md:border-r md:border-r-[#7C7F82]/20 border-t border-t-[#7C7F82]/20 md:border-t-0 w-full md:w-auto py-4 md:py-0 px-4 z-50">
+      class="bottom-0 fixed flex justify-center  bg-[#1c1c1c] h-auto border-t border-t-[#7C7F82]/20 w-full py-4 md:py-0 px-4 z-50">
       <nav>
-        <ul class="flex md:flex-col justify-center items-center space-x-4 md:space-y-4 md:space-x-0 h-full">
+        <ul class="flex justify-start p-4 space-x-4">
           <li>
-            <UTooltip text="Homepage" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/">
-                <UIcon name="i-solar-home-angle-broken" class="w-5 h-5" />
-              </NuxtLink>
-            </UTooltip>
+            <NuxtLink to="/" class="flex items-center space-x-2">
+              <UIcon name="i-solar-home-angle-broken" class="w-5 h-5 text-[#7C7F82]" />
+              <p class="hidden md:block text-[#7C7F82] text-sm">Homepage</p>
+            </NuxtLink>
           </li>
           <li>
-            <UTooltip text="All publications" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/publications">
-                <UIcon name="i-solar-book-2-broken" class="w-5 h-5" />
-              </NuxtLink>
-            </UTooltip>
+            <NuxtLink to="/publications" class="flex items-center space-x-2">
+              <UIcon name="i-solar-book-2-broken" class="w-5 h-5 text-[#7C7F82]" />
+              <p class="hidden md:block text-[#7C7F82] text-sm">Publications</p>
+
+            </NuxtLink>
           </li>
           <li>
-            <UTooltip text="Inspiration wall" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/inspiration">
-                <UIcon name="i-solar-album-broken" class="w-5 h-5" />
-              </NuxtLink>
-            </UTooltip>
+            <NuxtLink to="/inspiration" class="flex items-center space-x-2">
+              <UIcon name="i-solar-album-broken" class="w-5 h-5 text-[#7C7F82]" />
+              <p class="hidden md:block text-[#7C7F82] text-sm">Inspiration wall</p>
+
+            </NuxtLink>
           </li>
           <li>
-            <UTooltip text="Contact" :ui="ui" :popper="{
-              placement: 'right',
-            }">
-              <NuxtLink to="/contact">
-                <UIcon name="i-solar-mailbox-broken" class="w-5 h-5" />
-              </NuxtLink>
-            </UTooltip>
+            <NuxtLink to="/contact" class="flex items-center space-x-2">
+              <UIcon name="i-solar-mailbox-broken" class="w-5 h-5 text-[#7C7F82]" />
+              <p class="hidden md:block text-[#7C7F82] text-sm">Contact</p>
+            </NuxtLink>
           </li>
         </ul>
       </nav>

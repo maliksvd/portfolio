d15bfc68219c0bd37c07d5f3cce0907635a4d898
maliksvd <malik.krechiche@gmail.com>
2023-10-15 23:25:11 -0400

    fix: leading

--- components/Home/Introduction.vue
+++ components/Home/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section class="rounded-lg py-8 bg-[#1c1c1c] p-6 ring-[#282828] ring-1">
-      <h1 class="text-3xl md:text-4xl bg-gradient mb-6 gradient-text max-w-2xl leading-6">Creating innovative digital
+      <h1 class="text-3xl md:text-4xl bg-gradient mb-6 gradient-text max-w-2xl leading-10">Creating innovative digital
         experiences with SwiftUI, Nuxt, and Figma.</h1>
       <p class="text-[rgb(169,169,169)] text-base mb-8">Based in Canada</p>
       <UButton icon="i-solar-arrow-right-linear" label="Contact/Get in touch" color="black" size="lg" variant="ghost"

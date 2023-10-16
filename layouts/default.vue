156ba9332ef13e31433f0d52e30e50514baacb10
maliksvd <malik.krechiche@gmail.com>
2023-10-15 23:10:06 -0400

    fix: move animation to the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="w-full min-h-screen flex flex-col md:flex-row">
     <SideNavigation />
-    <main class="max-w-sm md:max-w-6xl mx-auto my-12">
+    <main class="main-wrapper opacity-0 max-w-sm md:max-w-6xl mx-auto my-12">
       <slot />
     </main>
   </div>
 </template>
 
+<script lang="ts" setup>
+
+const route = useRoute()
+const { $gsap } = useNuxtApp()
+
+console.log($gsap)
+
+if ($gsap) {
+  const animate = () => {
+    return $gsap.fromTo(".main-wrapper", { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1 })
+  }
+
+  onMounted(animate)
+  watch(() => route.path, animate)
+} else {
+  console.error("$gsap is undefined")
+}
+</script>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="homeWrapper opacity-0">
+  <div>
     <section>
       <div class='grid grid-cols-1 md:grid-cols-3 gap-8 mb-12'>
         <div class='md:col-span-2'>
@@ -20,15 +20,8 @@
 </template>
 
 <script lang="ts" setup>
-const { $gsap } = useNuxtApp()
 
-console.log($gsap)
-
-onBeforeMount(() => {
-  $gsap.fromTo('.homeWrapper', { opacity: 0, y: 100 }, { opacity: 1, y: 0, duration: 1.5 })
-})
 </script>
-
 
 
 <style></style>

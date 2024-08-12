70d467ced82cc23a53c534ee877671f50e3cb03c
Malik <62953363+maliksvd@users.noreply.github.com>
2024-08-12 00:48:25 -0400

    feat: add todo app

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,33 +1,31 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Projects() {
   const projectsItems = [
     {
-      title: "Optic Genius",
-      description:
-        "A platform that helps you to find the best eye-care professinals in your city.",
-      stack: ["React", "Next.js", "PostgreSQL"],
-      href: "https://opticgenius.ca",
+      title: 'Optic Genius',
+      description: 'A platform that helps you to find the best eye-care professinals in your city.',
+      stack: ['React', 'Next.js', 'PostgreSQL'],
+      href: 'https://opticgenius.ca',
     },
-  ];
+    {
+      title: 'm-todo',
+      description: 'A simple todo app built for exploring the power of localStorage and SessionStorage.',
+      stack: ['React', 'Next.js', 'Shadcn-UI', 'Framer Motion', 'localStorage'],
+      href: 'https://todo.maliksvd.ca/',
+    },
+  ]
   return (
     <div className="mt-24">
       <h2 className="mb-4 font-semibold">Projects</h2>
       <div className="grid grid-cols-1 gap-4">
         {projectsItems.map((item, index) => (
-          <Link
-            href={item.href}
-            key={index}
-            className="bg-[#151514] border border-[#262626] rounded-md w-full p-[16px] hover:bg-[#151514]/20"
-          >
+          <Link href={item.href} key={index} className="bg-[#151514] border border-[#262626] rounded-md w-full p-[16px] hover:bg-[#151514]/20">
             <h3 className="mb-4 font-semibold text-sm">{item.title}</h3>
             <p className="mb-2 text-[#8b8b8b] text-xs">{item.description}</p>
             <ul className="flex space-x-2 flex-wrap">
               {item.stack.map((stackItem, index) => (
-                <li
-                  key={index}
-                  className="bg-[#171717] border border-[#262626] rounded-md px-2 py-1.5 text-xs mt-2"
-                >
+                <li key={index} className="bg-[#171717] border border-[#262626] rounded-md px-2 py-1.5 text-xs mt-2">
                   {stackItem}
                 </li>
               ))}
@@ -36,5 +34,5 @@ export default function Projects() {
         ))}
       </div>
     </div>
-  );
+  )
 }
